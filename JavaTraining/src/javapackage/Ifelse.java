e621dacca0fd7c13c5354c7b9087d47426baac45
rohini909090 <rohnisri783@gmail.com>
2024-09-02 22:09:51 +0530

    commit javatask4

---
+++ JavaTraining/src/javapackage/Ifelse.java
@@ -0,0 +1,18 @@
+package javapackage;
+
+import java.util.Scanner;
+public class Ifelse {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner =new Scanner(System.in);
+		System.out.println("Enter your Age");
+		int age=scanner.nextInt();
+	}
+		
+	if (isEligible(age)) {
+		
+		System.out.println()
+	}
+
+}
