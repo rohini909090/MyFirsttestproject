d0e3d28d9d991f61fcacb34770dc2411c19b227e
rohini909090 <rohnisri783@gmail.com>
2024-09-02 22:50:07 +0530

    Charc A to Z Java

---
+++ TASK4/src/javapackage2/Char.java
@@ -0,0 +1,23 @@
+package javapackage2;
+
+import java.util.Scanner;
+
+public class Char {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("The Alphabets from A to Z are: ");
+		sc.nextLine();		
+		
+		for(char ch='A';ch<='Z';ch++)
+		
+		{
+			System.out.println(ch+" ");
+			
+		}
+		
+		}
+	
+	}
+
