aea068a6cb7d6644c8877a9fc4f493121ccfbb36
rohini909090 <rohnisri783@gmail.com>
2024-09-03 00:54:04 +0530

    Print 10 times

---
+++ JavaTraining/src/Execption/Print.java
@@ -0,0 +1,17 @@
+package Execption;
+
+
+
+public class Print {
+
+	public static void main(String[] args) {
+		
+		String message="Welcome to Guvi";
+		
+		for(int i=0;i<10;i++) {
+			System.out.println(message);
+		}
+		
+	}
+
+}
---
+++ JavaTraining/src/Execption/StringLength.java
@@ -0,0 +1,18 @@
+package Execption;
+
+
+
+public class StringLength {
+
+	public static void main(String[] args) {
+		
+	   String message="Guvi Geek";
+		
+		int length=message.length();
+		
+		System.out.println("The Length of the String:" +length);
+		
+
+	}
+
+}
